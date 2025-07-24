34ecfc66ae1b4af13f81f8f3f1d659f120a064bb
pedropelaes <pedro.pm2@puccampinas.edu.br>
2025-07-24 17:55:19 -0300

    fix: ajustes pre-release

--- Conta_Certa/conta_certa/lib/screens/settings.dart
+++ Conta_Certa/conta_certa/lib/screens/settings.dart
@@ -167,7 +167,6 @@ class _SettingsState extends State<Settings> {
                       ),
                     ),
                   ]
-                  
                 ),
               ),
             ),
