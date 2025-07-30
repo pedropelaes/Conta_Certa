c14ffbb1f112664427ca21851fe0c4096513323c
pedropelaes <pedro.pm2@puccampinas.edu.br>
2025-07-30 16:44:43 -0300

    Revert "chore: versionamento"
    This reverts commit 7b3a8824795e32e9902847faaf33435a9099a128.

--- Conta_Certa/conta_certa/android/app/build.gradle.kts
+++ Conta_Certa/conta_certa/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.pelaes.conta_certa"
+    namespace = "com.example.conta_certa"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.pelaes.conta_certa"
+        applicationId = "com.example.conta_certa"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
